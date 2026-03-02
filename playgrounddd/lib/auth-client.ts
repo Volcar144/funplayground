6e9514e260b50958e3c1041736fd15954d0b51a2
Danng <archiemclellan.author@gmail.com>
2026-03-02 13:43:03 +0000

    Fix

--- playgrounddd/lib/auth-client.ts
+++ playgrounddd/lib/auth-client.ts
@@ -2,8 +2,5 @@ import { createAuthClient } from "better-auth/react"
 import { sentinelClient } from "@better-auth/infra/client";
 
 export const authClient = createAuthClient({
-    plugins:[
-        sentinelClient()
-    ]
     
 })
--- playgrounddd/lib/auth.ts
+++ playgrounddd/lib/auth.ts
@@ -27,7 +27,6 @@ export const auth = betterAuth({
     plugins:[
         haveIBeenPwned(),
         nextCookies(),
-        dash()
     ],
 
 
