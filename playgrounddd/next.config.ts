ac5ca3e52f3976ed82e17d8e1cbec4fd5d92ebe9
Danng <archiemclellan.author@gmail.com>
2026-03-08 09:18:22 +0000

    add proxy

--- playgrounddd/next.config.ts
+++ playgrounddd/next.config.ts
@@ -4,18 +4,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: { serverMinification: false},
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: `${process.env.NEXT_PUBLIC_POSTHOG_HOST}/static/:path*`,
-      },
-      {
-        source: "/ingest/:path*",
-        destination: `${process.env.NEXT_PUBLIC_POSTHOG_HOST}/:path*`,
-      },
-    ];
-  },
   skipTrailingSlashRedirect: true,
 };
 
