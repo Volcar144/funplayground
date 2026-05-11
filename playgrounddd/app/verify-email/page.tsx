99aac9f083b10df902b40096482865b9f214b47f
Danng <archiemclellan.author@gmail.com>
2026-05-11 11:37:01 +0000

    Scaffold email verify page

---
+++ playgrounddd/app/verify-email/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function ForgotPasswordPage(){
+
+    return (
+        <main>
+            <div className="min-h-screen w-full max-w overflow-hidden">
+                <Image className="bg-img" alt="bg-img" src=""/>
+            </div>
+        </main>
+    )
+}
