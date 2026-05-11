436a49faf1514ba5e24b14c939d2fad4b543e5cf
Danng <archiemclellan.author@gmail.com>
2026-05-11 15:44:26 +0000

    feat:more updates to verify email page

--- playgrounddd/app/verify-email/page.tsx
+++ playgrounddd/app/verify-email/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
 
 export default function ForgotPasswordPage(){
@@ -7,12 +7,18 @@ export default function ForgotPasswordPage(){
         <main>
             <div className="min-h-screen w-full max-w overflow-hidden">
                 <Image className="bg-img" alt="bg-img" src="shapes1.svg"/>
-                <div></div>
-                <div className="flex flex-col gap-4 align-center items-center justify-center text-white font-sans">
-                    <Card className="bg-black rounded-md">
+                <div className="flex flex-grid gap-2">
+                    <Image src="logo.svg" height={16} width={16} alt="logo" />
+                    <p className="text-xl">Playground<b className="text-xl text-bold">dd</b></p>
+                </div>
+                <div className="flex flex-col gap-4 align-center items-center h-screen justify-center text-white font-sans">
+                    <Card className="bg-black rounded-sm w-full ">
                         <CardHeader>
                             <CardTitle>Verify your email</CardTitle>
                         </CardHeader>
+                        <CardContent>
+                            
+                        </CardContent>
                     </Card>
                 </div>
             </div>
