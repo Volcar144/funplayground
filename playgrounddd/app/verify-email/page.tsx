5ba140d39b43627f0c66bdb822a6d8dcd833fd1c
Danng <archiemclellan.author@gmail.com>
2026-05-11 15:47:55 +0000

    Update UI

--- playgrounddd/app/verify-email/page.tsx
+++ playgrounddd/app/verify-email/page.tsx
@@ -7,17 +7,17 @@ export default function ForgotPasswordPage(){
         <main>
             <div className="min-h-screen w-full max-w overflow-hidden">
                 <Image className="bg-img" alt="bg-img" src="shapes1.svg"/>
-                <div className="flex flex-grid gap-2">
-                    <Image src="logo.svg" height={16} width={16} alt="logo" />
-                    <p className="text-xl">Playground<b className="text-xl text-bold">dd</b></p>
+                <div className="flex flex-grid gap-2 text-white">
+                    <Image src="logo.svg" height={32} width={32} alt="logo" />
+                    <p className="text-xl">Playgrounddd</p>
                 </div>
                 <div className="flex flex-col gap-4 align-center items-center h-screen justify-center text-white font-sans">
-                    <Card className="bg-black rounded-sm w-full ">
+                    <Card className="bg-black rounded-sm">
                         <CardHeader>
                             <CardTitle>Verify your email</CardTitle>
                         </CardHeader>
                         <CardContent>
-                            
+
                         </CardContent>
                     </Card>
                 </div>
