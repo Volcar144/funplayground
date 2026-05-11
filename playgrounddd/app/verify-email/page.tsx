14814656e64d31e698e4049767ce0241469e06c3
Danng <archiemclellan.author@gmail.com>
2026-05-11 11:46:12 +0000

    Scaffold UI

--- playgrounddd/app/verify-email/page.tsx
+++ playgrounddd/app/verify-email/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
 
 export default function ForgotPasswordPage(){
@@ -6,6 +7,14 @@ export default function ForgotPasswordPage(){
         <main>
             <div className="min-h-screen w-full max-w overflow-hidden">
                 <Image className="bg-img" alt="bg-img" src="shapes1.svg"/>
+                <div></div>
+                <div className="flex flex-col gap-4 align-center items-center justify-center text-white font-sans">
+                    <Card className="bg-black rounded-md">
+                        <CardHeader>
+                            <CardTitle>Verify your email</CardTitle>
+                        </CardHeader>
+                    </Card>
+                </div>
             </div>
         </main>
     )
